d92bdb5ca34366d88da9824e291522cd5ea6ffd5
Taylor <taylor@taylors-mbp.mynetworksettings.com>
2024-04-23 18:35:29 -0400

    Edits the font sizes

--- src/Components/Languages/Languages.js
+++ src/Components/Languages/Languages.js
@@ -4,13 +4,13 @@ function Languages() {
   return (
     <div className="languages">
       <h1 className="lang-title">Languages I know</h1>
-      <h3>JavaScript</h3>
-      <h3>CSS</h3>
-      <h3>HTML</h3>
-      <h3>React</h3>
-      <h3>TypeScript</h3>
-      <h3>SQL</h3>
-      <h3>Node.Js</h3>
+      <h3 className="lang">JavaScript</h3>
+      <h3 className="lang">CSS</h3>
+      <h3 className="lang">HTML</h3>
+      <h3 className="lang">React</h3>
+      <h3 className="lang">TypeScript</h3>
+      <h3 className="lang">SQL</h3>
+      <h3 className="lang">Node.Js</h3>
     </div>
   );
 }
