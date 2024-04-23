f19dc0fdef325979685f96c2d5c0c238e9aaf780
Taylor <taylor@taylors-mbp.mynetworksettings.com>
2024-04-23 17:31:29 -0400

    Adds a portfolio page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 // import "./App.css";
 import Home from "./Components/Home/Home";
+import Portfolio from "./Components/Portfolio/Portfolio";
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <Portfolio />
     </div>
   );
 }
---
+++ src/Components/Portfolio/Portfolio.js
@@ -0,0 +1,13 @@
+import "./Portfolio.css";
+
+function Portfolio() {
+  return (
+    <div className="Portfolio">
+      <h3>Here are some projects I've worked on</h3>
+      <p>Bydget Bae</p>
+      <p>Rock The Code</p>
+    </div>
+  );
+}
+
+export default Portfolio;
