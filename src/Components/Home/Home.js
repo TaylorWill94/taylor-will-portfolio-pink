615b5488b1ec2c33b0e504e48d0d036773e99f8e
Taylor <taylor@taylors-mbp.mynetworksettings.com>
2024-04-23 17:24:37 -0400

    Setting up media queries

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,11 @@
+import "./Home.css";
+function Home() {
+  return (
+    <div className="Home">
+      <h1>Taylor Williams</h1>
+      <p>Software Engineer. Dog mom.</p>
+    </div>
+  );
+}
+
+export default Home;
