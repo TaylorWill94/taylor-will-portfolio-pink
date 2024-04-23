591014997b1090cd378638b99bd641a99934e635
Taylor <taylor@taylors-mbp.mynetworksettings.com>
2024-04-23 18:23:09 -0400

    Adds more languages and edits cursor

--- src/Components/Languages/Languages.js
+++ src/Components/Languages/Languages.js
@@ -5,8 +5,12 @@ function Languages() {
     <div className="languages">
       <h1 className="lang-title">Languages I know</h1>
       <h3>JavaScript</h3>
+      <h3>CSS</h3>
+      <h3>HTML</h3>
       <h3>React</h3>
       <h3>TypeScript</h3>
+      <h3>SQL</h3>
+      <h3>Node.Js</h3>
     </div>
   );
 }
