13419f23f7e3e0a151221741826921a4a12d36af
Taylor <taylor@taylors-mbp.mynetworksettings.com>
2024-04-23 23:32:52 -0400

    Updates the home bio

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,9 +4,9 @@ function Home() {
     <div className="Home">
       <h1 className="home-title">Taylor Williams</h1>
       <h3 className="home-desc">
-        Hi, my name is Taylor and I am fullstack developer based out of NYC. I
-        have been working as fullstack developer for over two years and have a
-        keen interest in frontend devleopment.
+        Hi, my name is Taylor and I am Fullstack Developer based out of NYC. I
+        have always had a passion for web development since the days of Blogger
+        (iykyk). I like to make beautiful websites with UI being the focus.
       </h3>
     </div>
   );
